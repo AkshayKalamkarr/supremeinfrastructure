24b6657cfe9c266d7fabadf8e539a660341a8723
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-14 10:38:20 +0530

    added supreme quote

--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
-import About from "@/components/About";
 import HeroSection from "@/components/HeroSection";
-import Image from "next/image";
+import SupremeQuote from "@/components/SupremeQuote";
+
 
 export default function Home() {
   return (
-   <>
-   <HeroSection/>
-   <About/>
-   </>
+    <>
+      <HeroSection />
+      <SupremeQuote />
+
+    </>
   );
 }
---
+++ components/SupremeQuote.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const SupremeQuote = () => {
+  return (
+    <div className='w-full h-screen absolute'>
+      <div className='bg-gray-300 h-3/6 w-full '>
+        <p className=' text-4xl justify-center items-start relative '>
+          Supreme infrastructure is engaged in providing various kind of civil construction
+          service that include Building construction, substation, cable trenching,
+          horticulture Garden design & Maintenance, interior designing, Turnkey projects,
+          Rain water Harvesting, Sewage waste management system, Government Liaising works.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default SupremeQuote
