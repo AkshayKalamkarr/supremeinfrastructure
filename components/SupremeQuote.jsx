36f7e833f13ceada185ada84a3e6c3da38809a65
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-14 11:29:48 +0530

    stamp placed

--- components/SupremeQuote.jsx
+++ components/SupremeQuote.jsx
@@ -1,19 +1,31 @@
-import React from 'react'
+import React from 'react';
 
 const SupremeQuote = () => {
   return (
-    <div className='w-full h-[500px] flex items-center justify-center bg-gray-100 relative overflow-hidden'>
-      <div className='absolute top-0 left-0 w-full h-full bg-gray-200 opacity-50 rounded-full scale-[2] transform -translate-x-1/4 -translate-y-1/4'></div>
-      <div className='max-w-4xl mx-4 z-10 animate-fade-in-up'>
-        <p className='text-2xl md:text-3xl lg:text-4xl text-center font-serif'>
-          Supreme infrastructure is engaged in providing various kind of civil construction
-          service that include Building construction, substation, cable trenching,
+    <div className="w-full h-[500px] flex items-center justify-center bg-gray-100 relative overflow-hidden">
+      {/* Background Circle */}
+      <div className="absolute top-0 left-0 w-full h-full bg-gray-200 opacity-50 rounded-full scale-[2] transform -translate-x-1/4 -translate-y-1/4"></div>
+
+      {/* Logo Image */}
+      <div className="absolute top-1/3 left-1/4 md:left-1/3 lg:left-1/4 opacity-20">
+        <img
+          src="/images/stamp.jpg"
+          alt="Logo"
+          className="w-[200px] h-[200px]"
+        />
+      </div>
+
+      {/* Quote Text */}
+      <div className="max-w-4xl mx-4 z-10 animate-fade-in-up">
+        <p className="text-2xl md:text-3xl lg:text-4xl text-center font-serif">
+          Supreme infrastructure is engaged in providing various kinds of civil construction
+          services that include Building construction, substation, cable trenching,
           horticulture Garden design & Maintenance, interior designing, Turnkey projects,
-          Rain water Harvesting, Sewage waste management system, Government Liaising works.
+          Rainwater Harvesting, Sewage waste management system, Government Liaising works.
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SupremeQuote
+export default SupremeQuote;
