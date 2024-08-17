7aa138f319b284690892eb8c89f3d7831aa87f7d
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-08-17 12:12:37 +0530

    feat: Add @tabler/icons-react dependency and configure lazy loading for images

--- app/page.js
+++ app/page.js
@@ -1,10 +1,10 @@
+import { AppleCardsCarouselDemo } from "@/components/AppleCards";
 import CareerswithSupreme from "@/components/CareerswithSupreme";
 import GetInTouch from "@/components/GetInTouch";
 import HeroSection from "@/components/HeroSection";
 import Portfolio from "@/components/Portfolio";
 import ServicesGrid from "@/components/Services";
 import SupremeQuote from "@/components/SupremeQuote";
-
 
 export default function Home() {
   return (
@@ -15,7 +15,7 @@ export default function Home() {
       <GetInTouch />
       <ServicesGrid />
       <CareerswithSupreme />
-
+      <AppleCardsCarouselDemo />
     </>
   );
 }
