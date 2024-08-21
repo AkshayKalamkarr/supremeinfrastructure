5ca1832b4d34d2320c861e6b070589813e038c67
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-21 10:01:32 +0530

    ne wcode

--- app/about/supremeInfrastructure/page.jsx
+++ app/about/supremeInfrastructure/page.jsx
@@ -96,7 +96,7 @@ const AboutUsPage = () => {
                 </section>
 
                 {/* Services Section */}
-                <div className='md:bg-white'>
+                <div>
                     <section className="mb-20">
                         <h2 className="text-3xl font-bold mb-12 text-center text-gray-800">Our Services</h2>
                         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
