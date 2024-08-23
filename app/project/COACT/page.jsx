12f8b76c63fbe1d6b3c0851bbc5c460d0cc2003f
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-23 15:48:08 +0530

    new code

--- app/project/COACT/page.jsx
+++ app/project/COACT/page.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const page = () => {
-  return (
-    <div className='text-red-500 bg-yellow-500 text-center mt-96'>
-      Coact
-    </div>
-  )
+    return (
+        <div className='text-red-500 bg-yellow-500 text-center mt-96'>
+            Coact
+        </div>
+    )
 }
 
 export default page
