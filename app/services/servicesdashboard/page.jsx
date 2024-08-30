703e20b9fa65e13a4b14070959230361000bd9a3
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-08-30 12:31:46 +0530

    new code

--- app/services/servicesdashboard/page.jsx
+++ app/services/servicesdashboard/page.jsx
@@ -124,8 +124,8 @@ export default function Home() {
               <button
                 onClick={() => handleCategoryClick(item)}
                 className={`w-full py-3 px-4 transition-all duration-300 ease-in-out text-center md:text-center md:my-4 ${selectedCategory.name === item.name
-                    ? 'bg-gray-200 text-gray-800 font-bold'
-                    : 'text-gray-600 hover:bg-gray-100'
+                  ? 'bg-gray-200 text-gray-800 font-bold'
+                  : 'text-gray-600 hover:bg-gray-100'
                   }`}
               >
                 {item.name}
