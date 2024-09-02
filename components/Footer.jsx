242071a31948ab6e4b2db165a1e1b31d3bd4c1c4
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-02 15:33:35 +0530

    new code

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
 
                     {/* Quick Links */}
                     <div className="space-y-4">
-                        <h2 className="text-xl sm:text-2xl font-bold uppercase border-b-2 border-yellow-500 pb-2 mb-4 text-center">Quick Links</h2>
+                        <h2 className="text-xl sm:text-2xl font-bold uppercase border-b-2 border-yellow-500 pb-2 mb-4 text-start">Quick Links</h2>
                         <div className="flex flex-col space-y-2">
                             {['About Us', 'Projects', 'Gallery', 'Contact'].map((item, index) => (
                                 <Link key={index} href={`/${item.toLowerCase().replace(' ', '')}`} className="text-gray-300 hover:text-yellow-500 transition duration-300 text-center text-sm sm:text-base hover:underline hover:underline-offset-4">
