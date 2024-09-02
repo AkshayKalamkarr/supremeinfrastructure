ec1f3f3f463c868610a48df248640378c458e51b
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-02 16:26:52 +0530

    new code

--- app/about/architecturalDesigns/page.jsx
+++ app/about/architecturalDesigns/page.jsx
@@ -200,7 +200,7 @@ export default function Home() {
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
               >
-                <Link href="/services/servicesdashboard" className="mt-4 inline-block bg-black text-white px-4 py-2">View Projects</Link>
+                <Link href="/services/servicesdashboard" className="mt-4 inline-block bg-black text-white px-4 py-2 md:mx-64">View Projects</Link>
               </motion.div>
             </div>
           </motion.div>
