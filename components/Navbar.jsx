7b7136ece8c1053917c03c7dff904760978bd72f
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-04 11:36:13 +0530

    new code

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -99,7 +99,7 @@ function Navbar({ className }) {
                     </MenuItem>
                     <Link href="/services/servicesdashboard"><MenuItem setActive={setActive} active={active} item="Services">
                     </MenuItem></Link>
-                    <Link href='/project'><MenuItem setActive={setActive} active={active} item="Project">
+                    <Link href='/project'><MenuItem setActive={setActive} active={active} item="Projects">
                         <div className="text-sm grid grid-cols-2 gap-10 p-4">
                             <ProductItem
                                 title="TATA"
