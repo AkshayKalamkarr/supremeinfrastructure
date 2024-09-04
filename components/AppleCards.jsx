8339c6cee3bb0f31f086fb588bfa782587f42f03
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-04 17:50:24 +0530

    new code

--- components/AppleCards.jsx
+++ components/AppleCards.jsx
@@ -11,7 +11,7 @@ export function AppleCardsCarouselDemo() {
   return (
     <div className='w-full h-full py-8 md:py-0 mt-22 md:mt-12 bg-white'>
       <div className='max-w-7xl mx-auto px-4'>
-        <h2 className='relative inline-block text-3xl md:text-5xl font-extrabold text-black font-sans text-center mb-4 md:mb-8 md:mx-96'>
+        <h2 className='relative inline-block text-3xl md:text-5xl font-extrabold text-black font-sans text-center sm:mx-12  mb-4 md:mb-8 md:mx-96'>
           Get to know Our Services
           <span className='absolute bottom-0 left-0 w-full h-1 bg-blue-600 transform scale-x-0 transition-transform duration-300 group-hover:scale-x-100'></span>
         </h2>
