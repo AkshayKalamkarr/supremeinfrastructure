76e82746d3eeb502cd4125f27007ad1f05a09c53
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-16 17:03:53 +0530

    new code

--- data/commercialprojects.js
+++ data/commercialprojects.js
@@ -1,11 +1,11 @@
 export const projects = [
   {
     slug: "project-1",
-    title: "SAI PRASAD GARDEN, Nevali, Panvel",
+    title: "TATA Mulshi",
     image: "/images/projects/ATLANTAS/atlantas-3.jpg",
     description: "Short description of Project 1",
     fullDescription:
-      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
+      "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
     reraNumber: "P52000011751",
     amenitiesDescription: "Description of amenities for Project 1",
     pdfLink: "/pdf/ongoing/ongoingProject-1.pdf",
