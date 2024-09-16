6a6913ab2c7b95ceb74f950a227a89bcf0dee168
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-16 18:16:41 +0530

    new code

--- data/commercialprojects.js
+++ data/commercialprojects.js
@@ -6,148 +6,31 @@ export const projects = [
     description: "Short description of Project 1",
     fullDescription:
       "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
-    reraNumber: "P52000011751",
-    amenitiesDescription: "Description of amenities for Project 1",
-    pdfLink: "/pdf/ongoing/ongoingProject-1.pdf",
-    amenities: [
-      { name: "Swimming Pool", image: "/images/amenities/pool.jpg" },
-      { name: "Car Parking", image: "/images/amenities/carParking.jpg" },
-      { name: "Indoor Games", image: "/images/amenities/game.jpg" },
-      { name: "Garden", image: "/images/amenities/garden.jpg" },
-      { name: "Gym", image: "/images/amenities/gym.jpg" },
-      { name: "Lift", image: "/images/amenities/lift.jpg" },
-    ],
-    floorplans: [
-      { title: '1st & 3rd Floor Plan', src: '/images/floorplans/ongoing/project-1/img-1.jpg' },
-      { title: '2nd & 4th Floor Plan', src: '/images/floorplans/ongoing/project-1/img-2.jpg' },
-      { title: 'Ground Floor Plan', src: '/images/floorplans/ongoing/project-1/img-3.jpg' },
-    ],
-    connectivity: [
-      "Panvel is a major transportation hub in Maharashtra, India, connecting various modes of transport such as railways, highways, and air travel. It serves as a junction for both local and long-distance travel",
-      "It is well-connected by major highways like the Mumbai-Pune Expressway (NH 48) and Sion-Panvel Expressway (NH 4B), facilitating smooth road transport to Mumbai, Pune, and other cities in Maharashtra.",
-      "Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses.",
-    ],
-    map: {
-      center: { lat: 19.009816908187975, lng: 73.10259316005418 },
-      zoom: 14,
-      markers: [
-        {
-          lat: 19.009816908187975,
-          lng: 73.10259316005418,
-          title: "Project 1 Location",
-        },
-      ],
-      iframeSrc:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin",
-    },
-    galleryImages: [
-      { image: "/images/projects/img-1.jpg", alt: "Exterior View" },
-      { image: "/images/projects/img-2.jpg", alt: "Living Room" },
-      { image: "/images/projects/img-3.jpg", alt: "Kitchen" },
-      { image: "/images/projects/img-4.jpg", alt: "Bedroom" },
-      { image: "/images/projects/img-1.jpg", alt: "Bathroom" },
-      { image: "/images/projects/img-2.jpg", alt: "Balcony View" },
-    ],
+    Client:"Tata Power",
+    ApproxArea:"1500",
+    ProjectDuration:"135 Days"
   },
   {
     slug: "project-2",
-    title: "JAYRAM RESIDENCY, Shedung, Panvel",
-    image: "/images/projects/COACT/coact-1.jpg",
+    title: "TATA Excellence Center",
+    image: "/images/projects/ATLANTAS/atlantas-3.jpg",
     description: "Short description of Project 2",
     fullDescription:
-      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
-    reraNumber: "P52000011751",
-    amenitiesDescription: "Description of amenities for Project 2",
-    pdfLink: "/pdf/ongoing/ongoingProject-2.pdf",
-    amenities: [
-      { name: "Swimming Pool", image: "/images/amenities/pool.jpg" },
-      { name: "Car Parking", image: "/images/amenities/carParking.jpg" },
-      { name: "Indoor Games", image: "/images/amenities/game.jpg" },
-      { name: "Garden", image: "/images/amenities/garden.jpg" },
-      { name: "Gym", image: "/images/amenities/gym.jpg" },
-      { name: "Lift", image: "/images/amenities/lift.jpg" },
-    ],
-    floorplans: [
-      { title: '1st & 3rd Floor Plan', src: '/images/floorplans/ongoing/project-1/img-1.jpg' },
-      { title: '2nd & 4th Floor Plan', src: '/images/floorplans/ongoing/project-1/img-2.jpg' },
-      { title: 'Ground Floor Plan', src: '/images/floorplans/ongoing/project-1/img-3.jpg' },
-    ],
-    connectivity: [
-      "Panvel is a major transportation hub in Maharashtra, India, connecting various modes of transport such as railways, highways, and air travel. It serves as a junction for both local and long-distance travel",
-      "It is well-connected by major highways like the Mumbai-Pune Expressway (NH 48) and Sion-Panvel Expressway (NH 4B), facilitating smooth road transport to Mumbai, Pune, and other cities in Maharashtra.",
-      "Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses.",
-    ],
-    map: {
-      center: { lat: 19.009816908187975, lng: 73.10259316005418 },
-      zoom: 14,
-      markers: [
-        {
-          lat: 19.009816908187975,
-          lng: 73.10259316005418,
-          title: "Project 1 Location",
-        },
-      ],
-      iframeSrc:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin",
-    },
-    galleryImages: [
-      { src: "/images/gallery/project-1/img-1.jpg", alt: "Exterior View" },
-      { src: "/images/gallery/project-1/img-2.jpg", alt: "Living Room" },
-      { src: "/images/gallery/project-1/img-3.jpg", alt: "Kitchen" },
-      { src: "/images/gallery/project-1/img-4.jpg", alt: "Bedroom" },
-      { src: "/images/gallery/project-1/img-5.jpg", alt: "Bathroom" },
-      { src: "/images/gallery/project-1/img-6.jpg", alt: "Balcony View" }
-    ],
+      "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
+    Client:"Tata Power",
+    ApproxArea:"1500",
+    ProjectDuration:"135 Days"
   },
   {
     slug: "project-3",
-    title: "NEW CITY PALACE, Pushapak Nagar, Panvel",
-    image: "/images/projects/TATA/tata-1.jpg",
+    title: "TATA Mulshi",
+    image: "/images/projects/ATLANTAS/atlantas-3.jpg",
     description: "Short description of Project 3",
     fullDescription:
-      "Ensuring buildings are constructed with strong foundations and materials to withstand environmental stresses and provide long-term safety and stability. Designing buildings with efficient layouts and amenities that cater to the needs of occupants while maximizing space utilization and comfort. Incorporating architectural features and finishes that enhance the visual appeal and character of the building, contributing to its overall attractiveness and identity.",
-    reraNumber: "P52000011751",
-    amenitiesDescription: "Description of amenities for Project 3",
-    pdfLink: "/pdf/ongoing/ongoingProject-3.pdf",
-    amenities: [
-      { name: "Swimming Pool", image: "/images/amenities/pool.jpg" },
-      { name: "Car Parking", image: "/images/amenities/carParking.jpg" },
-      { name: "Indoor Games", image: "/images/amenities/game.jpg" },
-      { name: "Garden", image: "/images/amenities/garden.jpg" },
-      { name: "Gym", image: "/images/amenities/gym.jpg" },
-      { name: "Lift", image: "/images/amenities/lift.jpg" },
-    ],
-    floorplans: [
-      { title: '1st & 3rd Floor Plan', src: '/images/floorplans/ongoing/project-1/img-1.jpg' },
-      { title: '2nd & 4th Floor Plan', src: '/images/floorplans/ongoing/project-1/img-2.jpg' },
-      { title: 'Ground Floor Plan', src: '/images/floorplans/ongoing/project-1/img-3.jpg' },
-    ],
-    connectivity: [
-      "Panvel is a major transportation hub in Maharashtra, India, connecting various modes of transport such as railways, highways, and air travel. It serves as a junction for both local and long-distance travel",
-      "It is well-connected by major highways like the Mumbai-Pune Expressway (NH 48) and Sion-Panvel Expressway (NH 4B), facilitating smooth road transport to Mumbai, Pune, and other cities in Maharashtra.",
-      "Panvel's strategic location places it near Mumbai's Chhatrapati Shivaji Maharaj International Airport and the upcoming Navi Mumbai International Airport, enhancing air connectivity for residents and businesses.",
-    ],
-    map: {
-      center: { lat: 19.009816908187975, lng: 73.10259316005418 },
-      zoom: 14,
-      markers: [
-        {
-          lat: 19.009816908187975,
-          lng: 73.10259316005418,
-          title: "Project 1 Location",
-        },
-      ],
-      iframeSrc:
-        "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d695.8197463799711!2d73.10259316005418!3d19.009816908187975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7e911b8186ff7%3A0x8637ac85995699fd!2sCrown%20Housing!5e1!3m2!1sen!2sin!4v1721728374695!5m2!1sen!2sin",
-    },
-    galleryImages: [
-      { src: "/images/gallery/project-1/img-1.jpg", alt: "Exterior View" },
-      { src: "/images/gallery/project-1/img-2.jpg", alt: "Living Room" },
-      { src: "/images/gallery/project-1/img-3.jpg", alt: "Kitchen" },
-      { src: "/images/gallery/project-1/img-4.jpg", alt: "Bedroom" },
-      { src: "/images/gallery/project-1/img-5.jpg", alt: "Bathroom" },
-      { src: "/images/gallery/project-1/img-6.jpg", alt: "Balcony View" },
-    ],
+      "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
+    Client:"Tata Power",
+    ApproxArea:"1500",
+    ProjectDuration:"135 Days"
   },
 ];
 
