65e755e7aaa8f52726e5f71ce5e2f701defc7074
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-18 12:52:54 +0530

    new code

--- components/GetInTouch.jsx
+++ components/GetInTouch.jsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const GetInTouch = () => {
   return (
     <div className="relative h-4/6 bg-cover bg-center bg-no-repeat" style={{backgroundImage: "url('/images/home/getinTouch.jpg')"}}>
-      <div className="absolute inset-0 bg-black bg-opacity-35  0"></div>
+      <div className="absolute inset-0 bg-black bg-opacity-35"></div>
       <div className="relative h-full md:h-[60vh] flex items-center justify-center px-4 sm:px-6 lg:px-8">
         <motion.div 
           className="text-center text-white p-6 max-w-2xl"
