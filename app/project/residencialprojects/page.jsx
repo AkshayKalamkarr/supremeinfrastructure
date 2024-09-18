ef090af1e3d20831bed81a25494e8b42fc902830
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-18 13:04:47 +0530

    new code

--- app/project/residencialprojects/page.jsx
+++ app/project/residencialprojects/page.jsx
@@ -47,13 +47,13 @@ const ProjectCard = ({ project }) => (
 const ResidencialProject = () => {
   return (
     <div>
-      <div className="bg-[url('/images/projects/residencialBackgroundWallpaper.jpg')] bg-center bg-cover h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] flex items-center justify-center">
-      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative bg-[url('/images/projects/residencialBackgroundWallpaper.jpg')] bg-center bg-cover h-[200px] sm:h-[300px] md:h-[400px] lg:h-[500px] flex items-center justify-center">
+        <div className="absolute inset-0 bg-black opacity-50"></div>
         <motion.h1
           initial='offscreen'
           whileInView={"onscreen"}
           variants={titleVariants}
-          className='container text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-white tracking-widest text-center'
+          className='container relative z-10 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-semibold text-white tracking-widest text-center'
         >
           Residencial Projects
         </motion.h1>
