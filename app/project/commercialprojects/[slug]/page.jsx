2bd379125d38e30cd807255c5a464a05dba550ad
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-09-28 16:16:58 +0530

    commercial project code and layout updated

--- app/project/commercialprojects/[slug]/page.jsx
+++ app/project/commercialprojects/[slug]/page.jsx
@@ -1,4 +1,5 @@
 import IFrameComponent from "@/components/IFrameComponent";
+import { TramFront, PlaneIcon, ComputerIcon, Building2Icon, SchoolIcon, TrainIcon, TrainFrontIcon, Building2, Home, Banknote, FileCheck, CheckSquare } from 'lucide-react';
 import { projects, getProjectBySlug } from "../../../../data/commercialprojects";
 import Image from "next/image";
 import Link from "next/link";
@@ -16,34 +17,125 @@ export default function ProjectPage({ params }) {
   if (!project) {
     return <div>Project not found</div>;
   }
+  const highlightsIcon = {
+    Building2: Building2,
+    Home: Home,
+    Banknote: Banknote,
+    FileCheck: FileCheck,
+    CheckSquare: CheckSquare
+  };
 
   return (
     <div className="bg-gradient-to-b from-white-100 to-white min-h-screen flex justify-center md:my-32">
-      <div className="w-full max-w-7xl px-4 sm:px-6 lg:px-8">
-        <section className=' py-8'>
-          <div className='flex flex-col lg:flex-row items-start'>
-            <div className='lg:w-1/2 lg:pr-6 text-center'>
-              <h1 className='text-2xl md:text-3xl font-bold mb-4 text-black text-center lg:text-center'>
-                {project.title}
-              </h1>
-              <p className='mb-4 text-sm md:text-base text-black'>
-                {project.fullDescription}
-              </p>
-            </div>
-            <div className='lg:w-1/2 mt-4 lg:mt-0'>
-              <Image
-                src={project.image}
-                alt={project.title}
-                width={600}
-                height={400}
-                className='w-full h-auto object-cover rounded-lg'
-              />
-            </div>
+    <div className="w-full px-4 sm:px-6 lg:px-8">
+      <section className='py-8 px-4 sm:px-6 lg:px-8'>
+        <div className='flex flex-col lg:flex-row items-start mx-auto w-full'>
+          <div className='lg:w-1/2 lg:pr-6 text-center'>
+            <h1 className='text-2xl md:text-3xl font-bold mb-4 text-black text-center lg:text-center md:my-8'>
+              {project.title}
+            </h1>
+            <p className='mb-4 text-sm md:text-base text-black lg:text-center'>
+              {project.fullDescription}
+            </p>
+            <p className='mb-4 text-sm md:text-lg font-semibold text-cyan-700 text-center lg:text-center md:mt-8'>
+              {project.tagLine}
+            </p>
+            
           </div>
-
+          <div className='lg:w-1/2 mt-4 lg:mt-0'>
+            <Image
+              src={project.image}
+              alt={project.title}
+              width={600}
+              height={400}
+              className='w-full h-auto object-cover rounded-lg'
+            />
+          </div>
+        </div>
+      </section>
+  
+      {project.galleryImages && (
+        <section className='bg-gray-100 py-12 px-4 sm:px-6 lg:px-8 w-full'>
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-xl mb-8 text-center text-cyan-700 md:my-6">
+            Gallery
+          </h2>
+          <div className="w-36 h-1 bg-gradient-to-r bg-orange-500 hover:bg-orange-600 mx-auto mb-8"></div>
+          <div className='grid grid-cols-2 md:grid-cols-4 gap-2 md:gap-4 md:mt-16 w-full'>
+            {project.galleryImages.map((galleryImage, index) => (
+              <div
+                key={index}
+                className='relative overflow-hidden aspect-square w-full'
+              >
+                <Image
+                  src={galleryImage.image}
+                  alt={galleryImage.name}
+                  fill
+                  sizes="(max-width: 768px) 50vw, (max-width: 1200px) 33vw, 25vw"
+                  className='object-cover transition duration-300 ease-in-out hover:scale-105'
+                />
+              </div>
+            ))}
+          </div>
         </section>
-
-      </div>
+      )}
+  
+      {project.highlights && (
+        <div className="bg-gray-200 py-12 px-4 sm:px-6 lg:px-8 w-full">
+          <h2 className="text-3xl md:text-4xl lg:text-5xl font-xl mb-8 text-center text-cyan-700 md:my-6">
+            Project Highlights
+          </h2>
+          <div className="w-36 h-1 bg-gradient-to-r bg-orange-500 hover:bg-orange-600 mx-auto mb-8"></div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8 w-full">
+            {project.highlights.map((item, index) => {
+              const IconComponent = highlightsIcon[item.icon];
+              return (
+                <div
+                  key={index}
+                  className="lg:my-8 flex flex-col items-center text-center bg-white p-6 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
+                >
+                  {IconComponent && <IconComponent className="w-8 h-8 mb-4 text-cyan-400" />}
+                  <p className="text-sm font-medium text-gray-900">{item.description}</p>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+  
+      {/* Configuration Section */}
+      <section className='bg-white py-12 px-4 sm:px-6 lg:px-8 w-full'>
+        <h2 className="text-3xl md:text-4xl lg:text-5xl font-xl mb-8 text-center text-cyan-700 md:my-6">
+          Configuration
+        </h2>
+        <div className="w-36 h-1 bg-gradient-to-r bg-orange-500 hover:bg-orange-600 mx-auto mb-8"></div>
+  
+        <div className="overflow-x-auto mb-12">
+          <table className="w-full border-collapse bg-white shadow-md rounded-lg">
+            <thead>
+              <tr className="bg-navy-700 text-black md:text-center">
+                <th className="py-3 px-4 text-left md:text-center">Approx Area</th>
+                <th className="py-3 px-4 text-left md:text-center">Project duration</th>
+                <th className="py-3 px-4 text-left md:text-center">Price</th>
+              </tr>
+            </thead>
+            <tbody>
+              {project.configuration.map((config, index) => (
+                <tr key={index} className="border-b border-gray-200 hover:bg-gray-100">
+                  <td className="py-3 px-4 md:text-center">{config.flat}</td>
+                  <td className="py-3 px-4 md:text-center">{config.carpet}</td>
+                  <td className="py-3 px-4 md:text-center">
+                    <Link href="/contact" className="bg-cyan-600 hover:bg-cyan-700 text-white font-bold py-2 px-4 rounded transition duration-300 ease-in-out">
+                      Get Quote
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </section>
     </div>
+  </div>
+  
   );
 }
--- data/commercialprojects.js
+++ data/commercialprojects.js
@@ -1,14 +1,36 @@
 export const projects = [
   {
     slug: "project-1",
-    title: "TATA Mulshi",
-    image: "/images/projects/ATLANTAS/atlantas-3.jpg",
-    description: "Short description of Project 1",
+    title: "TATA MULSHI",
+    image: "/images/projects/TATA/tata-1.jpg",
+    description: "TATA Project Discription",
     fullDescription:
       "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
-    Client:"Tata Power",
-    ApproxArea:"1500",
-    ProjectDuration:"135 Days"
+    tagLine: "Project information",
+    reraNumber: "P52000011751",
+    highlights: [
+      { icon: "Building2", description: "TOWNSHIP IN 3 ACRES" },
+      { icon: "Home", description: "13 RESIDENTIAL BUILDINGS OF STILT + 4 STOREY WITH LIFT" },
+      { icon: "Banknote", description: "LUXURIOUS AND AFFORDABLE 1BHK AND 2BHK RESIDENCES" },
+      { icon: "FileCheck", description: "PROJECT APPROVED BY NAINA CIDCO" },
+      { icon: "CheckSquare", description: "OC RECEIVED code" },
+    ],
+    configuration: [
+      { flat: "1500 sqft", carpet: "135 Days" },
+    ],
+
+    galleryImages: [
+      { image: "/images/projects/TATA/tata-1.jpg", alt: "tata-1" },
+      { image: "/images/projects/TATA/tata-2.jpg", alt: "tata-2" },
+      { image: "/images/projects/TATA/tata-3.jpg", alt: "tata-3" },
+      { image: "/images/projects/TATA/tata-4.jpg", alt: "tata-4" },
+      { image: "/images/projects/TATA/tata-5.jpg", alt: "tata-5" },
+      { image: "/images/projects/TATA/tata-6.jpg", alt: "tata-6" },
+      { image: "/images/projects/TATA/tata-7.jpg", alt: "tata-7" },
+      { image: "/images/projects/TATA/tata-8.jpg", alt: "tata-8" },
+      { image: "/images/projects/TATA/tata-9.jpg", alt: "tata-9" },
+      { image: "/images/projects/TATA/tata-10.jpg", alt: "tata-10" },
+    ],
   },
   {
     slug: "project-2",
@@ -17,9 +39,9 @@ export const projects = [
     description: "Short description of Project 2",
     fullDescription:
       "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
-    Client:"Tata Power",
-    ApproxArea:"1500",
-    ProjectDuration:"135 Days"
+    Client: "Tata Power",
+    ApproxArea: "1500",
+    ProjectDuration: "135 Days"
   },
   {
     slug: "project-3",
@@ -28,9 +50,9 @@ export const projects = [
     description: "Short description of Project 3",
     fullDescription:
       "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
-    Client:"Tata Power",
-    ApproxArea:"1500",
-    ProjectDuration:"135 Days"
+    Client: "Tata Power",
+    ApproxArea: "1500",
+    ProjectDuration: "135 Days"
   },
 ];
 
