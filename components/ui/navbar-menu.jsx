40b4cd300cf0c7ff4c69d3039b070c68e603ab4b
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-10-05 10:41:02 +0530

    code formate

--- components/ui/navbar-menu.jsx
+++ components/ui/navbar-menu.jsx
@@ -57,11 +57,9 @@ export const Menu = ({ setActive, children }) => {
         setIsHovered(false);
         setActive(null);
       }}
-      className={`relative rounded-full border border-transparent ${
-        isHovered ? 'bg-gradient-to-r from-gray-300 to-gray-200' : 'bg-transparent'
-      } transition-colors duration-300 flex justify-center md:text-xl space-x-16 px-8 py-6 mt-6 ${
-        isHovered ? 'shadow-3xl' : ''
-      }`}
+      className={`relative rounded-full border border-transparent ${isHovered ? 'bg-gradient-to-r from-gray-300 to-gray-200' : 'bg-transparent'
+        } transition-colors duration-300 flex justify-center md:text-xl space-x-16 px-8 py-6 mt-6 ${isHovered ? 'shadow-3xl' : ''
+        }`}
     >
       {children}
     </nav>
