d5867b5e72f033fac6b2feba662aff353e4c55be
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-10-28 11:03:54 +0530

    new code

--- app/project/residencialprojects/page.jsx
+++ app/project/residencialprojects/page.jsx
@@ -32,7 +32,7 @@ const ProjectCard = ({ project }) => (
         <p className="text-gray-600 text-xs sm:text-sm mb-4">{project.description}</p>
       </div>
       <Link href={`/project/residencialprojects/${project.slug}`}>
-        <span className="mt-auto w-full py-2 px-4 bg-gradient-to-r from-orange-600 to-orange-700 text-white rounded-md hover:from-orange-700 hover:to-orange-900 transition duration-300 text-xs sm:text-sm md:text-base inline-block text-center">
+        <span className="mt-auto w-full py-2 px-4 bg-gradient-to-r from-amber-700 to-yellow-600 text-white rounded-md hover:from-orange-700 hover:to-orange-900 transition duration-300 text-xs sm:text-sm md:text-base inline-block text-center">
           Read More
         </span>
       </Link>
