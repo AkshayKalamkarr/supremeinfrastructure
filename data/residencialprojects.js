245f74c24d7d3edd8fefb61564b9621deee4e86d
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-10-28 11:28:55 +0530

    new code

--- data/residencialprojects.js
+++ data/residencialprojects.js
@@ -1,9 +1,9 @@
 export const projects = [
     {
         slug: "project-1",
-        title: "Raheja",
+        title: "Raheja Acropolis",
         image: "/images/projects/TATA/tata-1.jpg",
-        description: "raheja",
+        description: "Devnar",
         fullDescription:
             "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
         tagLine: "Project information",
