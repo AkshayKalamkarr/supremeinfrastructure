71e762c5c0117025306cf02e37bff03651f84bcf
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-10-28 11:59:07 +0530

    new code

--- app/project/commercialprojects/[slug]/page.jsx
+++ app/project/commercialprojects/[slug]/page.jsx
@@ -106,7 +106,7 @@ export default function ProjectPage({ params }) {
             <h2 className="text-3xl md:text-4xl lg:text-5xl font-xl mb-8 text-center text-amber-700 md:my-6">
               Project Highlights
             </h2>
-            <div className="w-36 h-1 bg-gradient-to-r bg-cyan-500 hover:bg-cyan-600 mx-auto mb-8"></div>
+            <div className="w-36 h-1 bg-gradient-to-r bg-amber-500 hover:bg-cyan-600 mx-auto mb-8"></div>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-8 w-full">
               {project.highlights.map((item, index) => {
                 const IconComponent = highlightsIcon[item.icon];
@@ -115,7 +115,7 @@ export default function ProjectPage({ params }) {
                     key={index}
                     className="lg:my-8 flex flex-col items-center text-center bg-white p-6 rounded-lg shadow-md transition-transform duration-300 hover:scale-105"
                   >
-                    {IconComponent && <IconComponent className="w-8 h-8 mb-4 text-cyan-400" />}
+                    {IconComponent && <IconComponent className="w-8 h-8 mb-4 text-cyan-800" />}
                     <p className="text-sm font-medium text-gray-900">{item.description}</p>
                   </div>
                 );
