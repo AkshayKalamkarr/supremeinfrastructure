90b0c38b53131ad934ed47e27e8bed301988c663
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-11-05 11:58:32 +0530

    new code

--- data/residencialprojects.js
+++ data/residencialprojects.js
@@ -39,6 +39,62 @@ export const projects = [
             { image: "/images/projects/RAHEJA/raheja-17.jpeg", alt: "raheja-17" },
         ],
     },
+    {
+        slug: "project-2",
+        title: "Riddhi Siddhi 302",
+        image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-2.jpg",
+        description: "Ulwe",
+        fullDescription:
+            "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
+        tagLine: "Project information",
+        reraNumber: "P52000011751",
+        highlights: [
+            { icon: "Building2", description: "TOWNSHIP IN 3 ACRES" },
+            { icon: "Home", description: "13 RESIDENTIAL BUILDINGS OF STILT + 4 STOREY WITH LIFT" },
+            { icon: "Banknote", description: "LUXURIOUS AND AFFORDABLE 1BHK AND 2BHK RESIDENCES" },
+            { icon: "FileCheck", description: "PROJECT APPROVED BY NAINA CIDCO" },
+            { icon: "CheckSquare", description: "OC RECEIVED code" },
+        ],
+        configuration: [
+            { flat: "1500 sqft", carpet: "135 Days" },
+        ],
+
+        galleryImages: [
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-1.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-2.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-3.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-4.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-5.jpg", alt: "riddhisiddhi-1" },
+        ],
+    },
+    {
+        slug: "project-3",
+        title: "Riddhi Siddhi 702",
+        image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-1.jpg",
+        description: "Devnar",
+        fullDescription:
+            "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
+        tagLine: "Project information",
+        reraNumber: "P52000011751",
+        highlights: [
+            { icon: "Building2", description: "TOWNSHIP IN 3 ACRES" },
+            { icon: "Home", description: "13 RESIDENTIAL BUILDINGS OF STILT + 4 STOREY WITH LIFT" },
+            { icon: "Banknote", description: "LUXURIOUS AND AFFORDABLE 1BHK AND 2BHK RESIDENCES" },
+            { icon: "FileCheck", description: "PROJECT APPROVED BY NAINA CIDCO" },
+            { icon: "CheckSquare", description: "OC RECEIVED code" },
+        ],
+        configuration: [
+            { flat: "1500 sqft", carpet: "135 Days" },
+        ],
+
+        galleryImages: [
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-1.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-2.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-3.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-4.jpg", alt: "riddhisiddhi-1" },
+            { image: "/images/projects/RIDDHISIDDHI302/riddhisiddhi-5.jpg", alt: "riddhisiddhi-1" },
+        ],
+    },
 ];
 
 export const getProjectBySlug = (slug) => {
