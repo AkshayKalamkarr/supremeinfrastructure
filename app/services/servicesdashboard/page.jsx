2d865a9e621059e7eaf18540f4d6f6d82432c0b3
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-11-06 13:14:30 +0530

    portfolio text-4xl

--- app/services/servicesdashboard/page.jsx
+++ app/services/servicesdashboard/page.jsx
@@ -141,7 +141,7 @@ export default function Home() {
       {/* Sidebar */}
       <aside className="w-full lg:w-1/5 bg-amber-800 p-4 shadow-lg ">
         {/* <h1 className="text-3xl font-bold mb-2 text-center text-gray-800  md:my-24">Supreme Infrastructure Company</h1> */}
-        <h2 className="text-2xl font-bold mb-6 text-center text-white lg:my-12">PORTFOLIO / SERVICES</h2>
+        <h2 className="text-4xl font-bold mb-6 text-center text-white lg:my-12">PORTFOLIO</h2>
         <ul className="space-y-2">
           {portfolioItems.map((item, index) => (
             <li key={index} className="w-full">
