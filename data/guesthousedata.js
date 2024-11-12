d2f1268b0cd2f68dcca5e6d052ff76b643ceafdc
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-11-12 17:38:39 +0530

    new code

--- data/guesthousedata.js
+++ data/guesthousedata.js
@@ -99,7 +99,7 @@ export const guestprojects = [
   {
     slug: "project-4",
     title: "SENIOR CLUB TATA POWER SENIOR CLUB TROMBAY",
-    image: "/images/projects/TATAPOWERMULSHIIB/mulshi-2.jpg",
+    image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-1.jpeg",
     description: "TATA POWER SOLAR MULSHI",
     fullDescription:
       "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.The designing & decorating of structure is an important process as it is what makes a structure feel like a home. Basically Mulshi IB design is on that main aspect where guest will feel like their own house and will enjoy a bit time of their busy life",
@@ -117,12 +117,11 @@ export const guestprojects = [
     ],
 
     galleryImages: [
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-1.jpg", alt: "tata-1" },
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-3.jpg", alt: "tata-3" },
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-4.jpg", alt: "tata-4" },
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-5.jpg", alt: "tata-5" },
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-6.jpg", alt: "tata-6" },
-      { image: "/images/projects/TATAPOWERMULSHIIB/mulshi-7.jpg", alt: "tata-7" },
+      { image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-1.jpeg", alt: "tata-1" },
+      { image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-2.jpeg", alt: "tata-2" },
+      { image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-3.png", alt: "tata-3" },
+      { image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-4.png", alt: "tata-4" },
+      { image: "/images/projects/TATAPOWERSENIORCLUBTROMBAY/trombay-5.png", alt: "tata-5" },
     ],
   },
   {
