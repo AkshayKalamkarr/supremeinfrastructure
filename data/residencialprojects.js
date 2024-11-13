acd8156ad8bd4eda872d30642cb0ee702375dfc7
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-11-13 14:51:48 +0530

    raheja data update

--- data/residencialprojects.js
+++ data/residencialprojects.js
@@ -5,7 +5,7 @@ export const projects = [
         image: "/images/projects/RAHEJA/raheja-4.jpeg",
         description: "Devnar",
         fullDescription:
-            "Mulshi IB, situated about 40 km from Pune, is a storage dam for Bhira hydro power station of Tata Power Company Limited. The Bungalow is located near dam and dam constructed across the rivers Mula & Nila near village Mulshi during the years 1921-26 is a gravity dam constructed in rubble masonry.We have renovated the Mulshi IB with new design and concepts IN Year 2018-19. By keeping Bungalow’s vintage look in mind we have design this beautiful stone structure from scarp.This is Tata power’s oldest property among all. Bungalow home plans share a common style with Craftsman, Rustic and Cottage home designs. A great porch for your rocker, typically one level and over-hanging eaves are some of the classic features.",
+            "The Raheja Acropolis project is a 1,500-square-foot transformation, meticulously planned over 135 days to create a stylish and functional living space. Designed with thoughtful elements, the project features bay seating with built-in storage, optimizing space and comfort. Invisible grills provide an unobstructed view, adding a seamless connection to the outdoors. A mirror-finished, highlighted dining wall creates a sophisticated focal point, enhancing the aesthetic appeal of the area. Hidden doors and decorative partitions further elevate the interior, offering both privacy and elegance. This project merges modern design with practical solutions, crafting a refined, welcoming ambiance that redefines residential luxury.",
         tagLine: "Project information",
         reraNumber: "P52000011751",
         highlights: [
