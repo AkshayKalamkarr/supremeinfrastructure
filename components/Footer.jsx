2c880b9adff3361219dd6b71fc6939c92748b8b7
Akshay Kalamkar <kalamkarakshay2001@gmail.com>
2024-12-14 16:24:13 +0530

    new code

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -5,7 +5,7 @@ import { FaMapMarkerAlt, FaPhone, FaWhatsapp, FaEnvelope, FaGlobe, FaInstagram, 
 
 const Footer = () => {
     return (
-        <footer className="bg-gradient-to-r from-amber-700 via-yellow-700 to-amber-700 text-white">
+        <footer className="bg-gradient-to-r from-slate-900 via-slate-800 to-slate-900 text-amber-600">
             <div className="container mx-auto px-4 py-8 sm:py-12 md:py-16">
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-8 md:gap-12">
                     {/* Corporate Office */}
@@ -35,7 +35,7 @@ const Footer = () => {
                             ].map((item, index) => (
                                 <div key={index} className="flex items-center space-x-3 sm:space-x-7 group">
                                     <item.icon className="text-white-900 flex-shrink-0 transition-transform duration-300 group-hover:rotate-12" size={18} />
-                                    <p className="text-white-300 text-sm sm:text-base transition-colors duration-300 group-hover:text-black">{item.text}</p>
+                                    <p className="text-white-300 text-sm sm:text-base transition-colors duration-300 group-hover:text-yellow">{item.text}</p>
                                 </div>
                             ))}
                         </div>
@@ -53,7 +53,7 @@ const Footer = () => {
                             ].map((item, index) => (
                                 <Link key={index} href={item.href} className="flex items-center space-x-5 group">
                                     <item.icon className="text-white-500 flex-shrink-0 transition-transform duration-300 group-hover:rotate-12" size={18} />
-                                    <span className="text-white-300 hover:text-black transition duration-300 text-sm sm:text-base hover:underline hover:underline-offset-4">
+                                    <span className="text-white-300 hover:text-yellow transition duration-300 text-sm sm:text-base hover:underline hover:underline-offset-4">
                                         {item.text}
                                     </span>
                                 </Link>
@@ -66,7 +66,7 @@ const Footer = () => {
                         <h2 className="text-xl sm:text-xl font-bold uppercase border-b-2 border-gray-500 pb-2 mb-4 text-start">Connect With Us</h2>
                         <div className="flex md:mx-4 space-x-4">
                             {[FaInstagram, FaTwitter, FaLinkedin, FaFacebook].map((Icon, index) => (
-                                <Link key={index} href="/" className="text-white-300 hover:text-black transition duration-300 transform hover:-translate-y-1 hover:scale-110">
+                                <Link key={index} href="/" className="text-white-300 hover:text-yellow transition duration-300 transform hover:-translate-y-1 hover:scale-110">
                                     <Icon size={24} />
                                 </Link>
                             ))}
@@ -83,7 +83,7 @@ const Footer = () => {
                     </div>
                     <div className="space-x-4">
                         {['Privacy Policy', 'Terms of Service'].map((item, index) => (
-                            <Link key={index} className="text-white-400 hover:text-black transition duration-300 text-sm sm:text-base hover:underline hover:underline-offset-4" href="/">
+                            <Link key={index} className="text-white-400 hover:text-red transition duration-300 text-sm sm:text-base hover:underline hover:underline-offset-4" href="/">
                                 {item}
                             </Link>
                         ))}
